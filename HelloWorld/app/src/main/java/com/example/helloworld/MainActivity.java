32abe98c4a532cf1608378bcc06364c3ec46a85a
rayo100 <cv.1000458640@gmail.com>
2023-08-26 19:52:13 -0500

    Hello World

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
+}
